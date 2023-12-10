cd8b223ad2dfa111665831d43312a40205c6f32a
sfnm1 <sfnm1@student.le.ac.uk>
2023-12-10 17:59:52 +0000

    Added bash scripts for all testing techniques

---
+++ src/test/whitebox/statementBased/bash_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# To run this file go to this location in terminal, Then type: chmod +x bash_script.sh. Then run it by: ./bash_script.sh
+rm -rf ../../../../classes/
+
+# Please replace ';' with ':' incase of Linux and MacOS (Remove double quotes "" if required) or Run the .bat file
+javac -d ../../../../classes -cp "../../../../jars;../../../../src;../../../../junit-platform-console-standalone-1.10.1.jar" -g ../../../../src/main/java/placementApplicationTracker/mainMenu/*.java ../../../../src/main/java/placementApplicationTracker/adminMenu/*.java ../../../../src/main/java/placementApplicationTracker/model/*.java ../../../../src/main/java/placementApplicationTracker/repo/intf/*.java ../../../../src/main/java/placementApplicationTracker/service/*.java ../../../../src/main/java/placementApplicationTracker/studentMenu/*.java ../../../../src/test/whitebox/statementBased/*.java
+java -cp ../../../../classes -javaagent:../../../../jacoco-0.8.11/lib/jacocoagent.jar -jar ../../../../junit-platform-console-standalone-1.10.1.jar execute --class-path ../../../../classes --scan-class-path
+java -jar ../../../../jacoco-0.8.11/lib/jacococli.jar report jacoco.exec --classfiles ../../../../classes --sourcefiles ../../../../src --html report
